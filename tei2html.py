19e94653fd20b02c45fdfd29c74a88b927463c18
sarkozi_zsofia <zsofia.monika@gmail.com>
2022-05-19 18:08:44 +0200

    meta

--- tei2html.py
+++ tei2html.py
@@ -10,6 +10,7 @@ from multiprocessing import Pool
 
 from tei_to_html_utils import file_gen_from_zip, create_new_tag_with_string
 
+
 HTML = '<html><head itemscope itemtype="http://schema.org/NewsArticle"></head></html>'
 
 
@@ -39,11 +40,10 @@ FREE = {
 
 FREE_ATTRS = {
     'target': 'href',
-    'facs': 'a',
+    'facs': 'href',
     'type': 'class',
     'rend': 'class'}
     # tag.attrs['resp'] = 'script'
-
 
 
 # <head itemscope itemtype="http://schema.org/Article">
@@ -52,19 +52,45 @@ FREE_ATTRS = {
 #   <link itemprop="image" href="http://www.example.com/image.jpg">
 # </head>
 
+META_KEYS = {'sch:datePublished': 'Közzététel dátuma:', 'sch:dateModified': 'Módosítás dátuma:',
+             'sch:author': 'Szerzők:', 'sch:source': 'Forrás:', 'sch:articleSection': 'Rovat:',
+             'sch:keywords': 'Kulcsszavak:'}  # TODO: ezt kaphatná a main fv
+
+
+def get_article_data(bs_xml):
+    meta_d = {}
+    for m_key in META_KEYS.keys():  # {'sch:articleSection', 'sch:keywords'}
+        meta_val_list = [meta_val.text.strip() for meta_val in bs_xml.find_all(m_key)]
+        if len(meta_val_list) > 0:
+            meta_d[META_KEYS[m_key]] = ' ,'.join(meta_val_list)
+    return meta_d
+
 
 def fill_meta_block(input_bs, out_html):
     xenodata = input_bs.find('xenoData').find('rdf:Description')
     head = out_html.head
+    meta_for_human = out_html.new_tag('div', attrs={'class': 'meta'})
+    print(meta_for_human)
+    meta_dict = get_article_data(input_bs)
+    for mkey, mval in meta_dict.items():
+        a_meta = out_html.new_tag('b')
+        a_meta.string = f'{mkey} {mval}'
+        meta_for_human.append(a_meta)
+    for m in meta_for_human.find_all():
+        m.wrap(out_html.new_tag('p'))
     for x in xenodata.find_all():
         schema_org_meta = out_html.new_tag('meta', attrs={'itemprop': x.name, 'content': x.text.strip()})
         head.append(schema_org_meta)
+    return meta_for_human
 
 
 def change_body_tags(html_body):
     for tag in html_body.find_all():
         if tag.name == 'hi':
-            tag.name = HI_DICT.get(tag.attrs['rend'], 'em')
+            if 'rend' in tag.attrs.keys():
+                tag.name = HI_DICT[tag.attrs['rend']]
+            else:
+                tag.name = 'em'
             tag.attrs = {}
         elif tag.name == 'p' and tag.attrs.get('rend', None) == 'head':
             tag.name = 'h3'
@@ -96,20 +122,20 @@ def change_body_tags(html_body):
                 tag.attrs = new_attrs
 
 
-
 def process_and_print_one_article(xml_file):
     bs_xml = BeautifulSoup(xml_file, features='xml')
     # bs_html = BeautifulSoup(str(bs_xml.body), features='html.parser')
     bs_html = BeautifulSoup(HTML, features='html.parser')
     bs_html.head.insert_after(bs_xml.body)
-    fill_meta_block(bs_xml, bs_html)
+    meta_for_human_block = fill_meta_block(bs_xml, bs_html)
     change_body_tags(bs_html.body)
+    bs_html.h1.insert_after(meta_for_human_block)
     #print(bs_html)
     return bs_html
-    # TODO: os.makedirs(os.path.dirname(new_fold_file), exist_ok=True)
 
 
 def process_portal_zip_to_htmls(archive_path_fold, out_folder, selected, suf='_2022'):
+    print(archive_path_fold)
     out_folder_path = Path(out_folder)
     for archive in glob.iglob(f'{archive_path_fold}/*.zip'):
         zipname = archive[archive.rfind('/') + 1:]
@@ -136,7 +162,7 @@ if __name__ == '__main__':
     parser.add_argument('--selected_zips', type=str, required=False, default='all')
     args = parser.parse_args()
     process_portal_zip_to_htmls(args.inp_zip_dir, args.out_xml_dir, args.selected_zips)"""
-    inp_zip_dir = '/media/eltedh/6EAB565C0EA732DB/TEI_zips'
+    inp_zip_dir = '/home/dh/PycharmProjects/TEI2HTML'#/media/eltedh/6EAB565C0EA732DB/TEI_zips'
     out_xml_dir = 'HTMLs'
     selected_zips = ['mosthallottam.zip']
     process_portal_zip_to_htmls(inp_zip_dir, out_xml_dir, selected_zips)
@@ -147,7 +173,8 @@ if __name__ == '__main__':
         # 'magyaridok.zip',
         # 'valasz.zip',
         # 'vs.zip',
-        'mosthallottam.zip',
+        'vadhajtasok.zip'
+        # 'mosthallottam.zip',
         # 'epiteszforum.zip',
         # 'maszol.zip',
         # 'utazomajom.zip',
