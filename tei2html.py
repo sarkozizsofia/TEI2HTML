703b81eff3c5468ba4c27dad851b5bd5d0053539
SLZsofia <zsofia.monika@gmail.com>
2022-06-21 00:54:41 +0200

    .

--- tei2html.py
+++ tei2html.py
@@ -1,27 +1,12 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8, vim: expandtab:ts=4 -*-
 import glob
-import argparse
-import os
-import zipfile
 from bs4 import BeautifulSoup
 from pathlib import Path
-from multiprocessing import Pool
 
-from tei_to_html_utils import file_gen_from_zip, create_new_tag_with_string
+from tei_to_html_utils import file_gen_from_zip
 
 HTML = '<html><head itemscope itemtype="http://schema.org/NewsArticle"><style></style></script></head></html> '
-# <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous">
-# TODO CSS: note > hide
-# https://www.w3schools.com/icons/tryit.asp?filename=trybs_ref_glyph_camera
-# <div class="meta">
-# <i style='font-size:24px' class='fas'>&#xf083;</i> https://www.w3schools.com/icons/icons_reference.asp
-
-# .social-sharing-icons li.pinterest-share:before{
-# 	content: '\f16d';
-# }
-# border-image-source: url(/media/diamonds.png);
-
 
 HI_DICT = {
     'bold': 'b',
@@ -55,16 +40,13 @@ FREE_ATTRS = {
     'facs': 'src',
     'type': 'type',
     'rend': 'class'}
-#  tag.attrs['resp'] = 'script'
 
 META_KEYS = {'sch:datePublished': 'Közzététel dátuma:', 'sch:dateModified': 'Módosítás dátuma:',
              'sch:author': 'Szerzők:', 'sch:source': 'Forrás:', 'sch:articleSection': 'Rovat:',
              'sch:keywords': 'Kulcsszavak:', 'sch:url': 'URL:'}
 
 
-def get_article_data(bsxml, uuid):
-    #if uuid == '61989946-eb03-5559-b68c-d5db60b2d3aa':
-    #    print(bsxml)
+def get_article_data(bsxml):
     meta_d = {}
     for m_key in META_KEYS.keys():
         meta_val_list = [meta_val.text.strip() for meta_val in bsxml.find_all(m_key)]
@@ -75,36 +57,30 @@ def get_article_data(bsxml, uuid):
                 meta_d[META_KEYS[m_key]] = ', '.join(meta_val_list)
     if bsxml.find('div', {'type': 'page'}) is not None:
         urls = [u.attrs['source'] for u in bsxml.find_all('div', {'type': 'page'})]
-        meta_d[META_KEYS['URL:']] = '\n'.join(urls)
-        print(urls)
+        meta_d['URL:'] = '\n'.join(urls)
     return meta_d
 
 
-def fill_meta_block(input_bs, out_html, a_uuid):
+def fill_meta_block(input_bs, out_html):
     xenodata = input_bs.find('xenoData').find('rdf:Description')
     head = out_html.head
     meta_for_human = out_html.new_tag('div', attrs={'class': 'meta'})
-    # print(meta_for_human)
-    meta_dict = get_article_data(input_bs, a_uuid)
+    meta_dict = get_article_data(input_bs)
     for mkey, mval in meta_dict.items():
-        a_meta = out_html.new_tag('p')  # a_meta = out_html.new_tag('b')
+        a_meta = out_html.new_tag('p')
         a_meta.string = f'{mkey} {mval}'
         meta_for_human.append(a_meta)
-    """for m in meta_for_human.find_all():
-        m.wrap(out_html.new_tag('p'))"""
     for x in xenodata.find_all():
         schema_org_meta = out_html.new_tag('meta', attrs={'itemprop': x.name, 'content': x.text.strip()})
         head.append(schema_org_meta)
     return meta_for_human
 
 
-def validate_html(html_obj, url):
+def validate_html(html_obj):
     #  <figure class="media_content" src="https://www.mosthallottam.hu/wp-content/uploads/2020/11/ev-rovara-2021.jpg">
     for fig in html_obj.find_all('figure'):
         if 'src' in fig.attrs.keys():
             # embedded_content
-            """if fig.attrs['class'] == 'embedded_content':
-                fig.name = 'iframe'"""
             button = html_obj.new_tag('button', {'style': 'font-size:40px'})
             new_img = html_obj.new_tag('a', attrs={'href': fig['src']})
             new_img.string = fig['src']
@@ -115,19 +91,9 @@ def validate_html(html_obj, url):
             else:
                 fig.append(button)
             del fig['src']
-    for p in html_obj.find_all('div', {'class': 'page'}):
-        print(url)
 
 
-# <figure rend="media_content">
-# <figure rend="diagram">
-# <figure rend="embedded_content">
-# <floatingText type="social_media_content">
-# <figure rend="embedded_social_media_content"
-# <div type="page" "source"=#URL>
-
-
-def change_body_tags(bs_html, f_uuid):
+def change_body_tags(bs_html):
     html_body = bs_html.body
     for tag in html_body.find_all():
         if tag.name == 'hi':
@@ -150,46 +116,39 @@ def change_body_tags(bs_html, f_uuid):
         else:
             tag_name = tag.name
             tag_attrs = tag.attrs
-            #if tag_name == 'div' and 'type' in tag_attrs.keys() and tag_attrs['type'] == 'feed':
-                #print('>>>FEED', f_uuid)
             isname = FREE.get(tag_name)
             if isname is not None:
                 tag.name = isname
             if tag_attrs != {}:
-                # <figure facs="https://ad.adverticum.net/t/?z=6653496&amp;g=6653497&amp;b=665349900&amp;h=%5BLOCATION%5D&amp;p=2" rend="embedded_content" resp="script" type="corrected">
+                # <figure facs="https://ad.adverticum.net/t/?z=6653496&amp;g=6653497&amp;b=665349900&amp;h
+                # =%5BLOCATION%5D&amp;p=2" rend="embedded_content" resp="script" type="corrected">
                 new_attrs = {}
                 for k, v in tag_attrs.items():
                     if k in FREE_ATTRS:
                         new_attrs[FREE_ATTRS[k]] = v
                     else:
-                        # print('WHAT?', k, v, tag)
                         new_attrs[k] = v
                 tag.attrs = new_attrs
             if tag.name == 'div':
                 if 'type' in tag.attrs.keys():
                     tag.attrs['class'] = tag.attrs['type']
                     del tag.attrs['type']
-                    # <floatingText type='frame'>
-                    # <floatingText type='lead'>
-
     for tag in html_body.find_all('to_unwrap'):
         tag.unwrap()
-    validate_html(bs_html, f_uuid)
+    validate_html(bs_html)
 
 
-def process_and_print_one_article(xml_file, uu):
+def tei_to_html(xml_file, u):
     bs_xml = BeautifulSoup(xml_file, features='xml')
-    # bs_html = BeautifulSoup(str(bs_xml.body), features='html.parser')
-    if uu == '61989946-eb03-5559-b68c-d5db60b2d3aa':
+    if u =='0a9cd21c-8747-5636-89b5-f987d31e1d3d':
         print(bs_xml)
     bs_html = BeautifulSoup(HTML, features='html.parser')
     css = open('tei2html.css', 'r')
     bs_html.style.string = css.read()
+    meta_for_human_block = fill_meta_block(bs_xml, bs_html)
     bs_html.head.insert_after(bs_xml.body)
-    meta_for_human_block = fill_meta_block(bs_xml, bs_html, uu)
-    change_body_tags(bs_html, uu)
+    change_body_tags(bs_html)
     bs_html.h1.insert_after(meta_for_human_block)
-    # print(bs_html)
     return bs_html
 
 
@@ -200,21 +159,15 @@ def process_portal_zip_to_htmls(archive_path_fold, out_folder, selected, suf='_2
         zipname = archive[archive.rfind('/') + 1:]
         if zipname in selected or selected == 'all':
             portal_name = zipname[:zipname.find('.')]
-            # sketchfile_name = f'{sketchfile_folder}/{portal_name}_{suf}.xml'
             for z_obj, filepath in file_gen_from_zip(archive):
                 with z_obj.open(filepath) as text_f:
                     uuid = filepath[filepath.find('/') + 1:filepath.find('.')]
                     filename_uuid = f'{uuid}.html'
-                    article_as_html = process_and_print_one_article(text_f, uuid)
+                    article_as_html = tei_to_html(text_f, uuid)
                     html_filename = out_folder_path / portal_name / filename_uuid
-                    html_filename.parent.mkdir(parents=True, exist_ok=True)  # Create output dirs on the fly
+                    html_filename.parent.mkdir(parents=True, exist_ok=True)
                     with open(html_filename, 'w', encoding='UTF-8') as output_html:
                         print(article_as_html.prettify(), file=output_html)
-                        # pretty_xml = prettify_beta(str(article_as_html))
-                        # print(pretty_xml, file=output_html)
-
-
-# TODO: INFOGRAM: file:///home/eltedh/PycharmProjects/TEI2HTML/HTMLs/abcug/263ae88e-93e9-566f-ac11-df581b46af4b.html
 
 
 if __name__ == '__main__':
